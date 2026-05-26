cec632a8b9466ca41f0e9c67610794ee1a822a44
l3anxna <patthadonaroonpairodjanakul@gmail.com>
2026-05-26 16:16:12 +0700

    REFACTOR: CRUD for auth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\ShowController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
+    Route::get('/', function () {
+        return view('admin.dashboard');});
+    Route::resource('shows', ShowController::class);
+});
+
+require __DIR__.'/auth.php';
