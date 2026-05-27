d4807abaf1d30c4f3e98af0aeb89dcdbef2ee457
l3anxna <patthadonaroonpairodjanakul@gmail.com>
2026-05-27 18:03:37 +0700

    REFACTOR: admin dashboard route

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,29 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\ActorController;
 use App\Http\Controllers\Admin\ShowController;
+use App\Http\Controllers\Admin\VenueController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'admin'])
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function () {
 
-Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
-    Route::get('/', function () {
-        return view('admin.dashboard');});
-    Route::resource('shows', ShowController::class);
-});
+        Route::get('/', function () {
+            return view('admin.dashboard');
+        })->name('dashboard');
+
+        Route::resource('shows', ShowController::class);
+
+        Route::resource('actors', ActorController::class);
+
+        Route::resource('venues', VenueController::class);
+
+    });
 
 require __DIR__.'/auth.php';
