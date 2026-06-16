e4fccfbb91bd1eeb3dd792746f94890337d3559c
l3anxna <patthadonaroonpairodjanakul@gmail.com>
2026-06-16 14:06:47 +0700

    REFACTOR: route for actor page, show page and venue page

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\Admin\ActorController;
 use App\Http\Controllers\Admin\ShowController;
 use App\Http\Controllers\Admin\VenueController;
+use App\Http\Controllers\ShowPageController;
+use App\Http\Controllers\ActorPageController;
+use App\Http\Controllers\VenuePageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -25,5 +28,22 @@ Route::middleware(['auth', 'admin'])
         Route::resource('venues', VenueController::class);
 
     });
+
+Route::get('/', [ShowPageController::class, 'index']);
+
+Route::get(
+    '/shows/{slug}',
+    [ShowPageController::class, 'show']
+);
+
+Route::get(
+    '/actors/{slug}',
+    [ActorPageController::class, 'show']
+);
+
+Route::get(
+    '/venues/{slug}',
+    [VenuePageController::class, 'show']
+);
 
 require __DIR__.'/auth.php';
