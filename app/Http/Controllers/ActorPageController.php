8e98a7e1ebdff3640a71581baa2085345daff774
l3anxna <patthadonaroonpairodjanakul@gmail.com>
2026-08-09 16:02:44 +0700

    REFACTOR: Fix Actor and Venue Controller

--- app/Http/Controllers/ActorPageController.php
+++ app/Http/Controllers/ActorPageController.php
@@ -3,18 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Actor;
 
 class ActorPageController extends Controller
 {
+    public function index()
+    {
+        $actors = Actor::with('shows')->get();
+
+        return view('actors.index', compact('actors'));
+    }
+
     public function show($slug)
     {
         $actor = Actor::with('shows')
             ->where('slug', $slug)
             ->firstOrFail();
 
-        return view(
-            'actors.show',
-            compact('actor')
-        );
+        return view('actors.show',compact('actor'));
     }
 }
--- app/Http/Controllers/VenuePageController.php
+++ app/Http/Controllers/VenuePageController.php
@@ -2,19 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Venue;
 
 class VenuePageController extends Controller
 {
+    public function index()
+    {
+        $venues = Venue::with('shows')->get();
+
+        return view('venues.index', compact('venues'));
+    }
+
     public function show($slug)
     {
         $venue = Venue::with('shows')
             ->where('slug', $slug)
             ->firstOrFail();
 
-        return view(
-            'venues.show',
-            compact('venue')
-        );
+        return view('venues.show', compact('venue'));
     }
 }
